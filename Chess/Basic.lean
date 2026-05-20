6aa361ca16f2ca0cd3fa9ec7bfe51d20de66484a
Bergschaf <christian.k@netcom-mail.de>
2026-05-20 16:24:06 +0200

    CounterM (schlecht)

--- Chess/Basic.lean
+++ Chess/Basic.lean
@@ -20,26 +20,34 @@ end Evalutation
 
 def bigScore := 200
 
-def getScore (b : Board) (player : Turn) (t : Turn) (depth : Nat) : Int :=
+structure CounterM (α : Type) where
+  val : α
+  count : Nat := 0
+deriving Repr
+
+instance : Monad CounterM where
+  pure a := ⟨a, 1⟩
+  bind ma f := let x := f ma.val; ⟨x.val, x.count + ma.count⟩
+
+
+def getScore (b : Board) (player : Turn) (t : Turn) (depth : Nat) : CounterM Int := do
   match depth with
-  | 0 => b.evaluate player
+  | 0 => pure <| b.evaluate player
   | .succ n =>
     if t = player then
-      (b.possibleMoves t).foldl (fun acc m ↦
-      if acc > bigScore then acc else
-      max acc (getScore (b.applyMove m) player t.next n)) (-1000)
+      (b.possibleMoves t).foldlM (fun (acc : Int) (m : Move) ↦
+      if acc > bigScore then pure acc else (max acc) <$> (getScore (b.applyMove m) player t.next n)) (-1000 : Int)
     else
-      (b.possibleMoves t).foldl (fun acc m ↦
-       if -acc > bigScore then acc else
-       min acc (getScore (b.applyMove m) player t.next n)) 1000
+      (b.possibleMoves t).foldlM (fun acc m ↦
+      if -acc > bigScore then pure acc else (min acc) <$> (getScore (b.applyMove m) player t.next n)) (1000 : Int)
 
 /-- TODO Was wenn keine Moves -/
-def Board.bestMove (b : Board) (t : Turn) (depth : Nat) : Move × Int :=
-  (b.possibleMoves t).map (fun m ↦ (m,getScore (b.applyMove m) t t.next depth)) |>.maxOn? (fun mt ↦ mt.2) |>.get!
+def Board.bestMove (b : Board) (t : Turn) (depth : Nat) : Move × (CounterM Int) :=
+  (b.possibleMoves t).map (fun m ↦ (m,getScore (b.applyMove m) t t.next depth)) |>.maxOn? (fun mt ↦ mt.2.val) |>.get!
 
 def TestBoard1 := FENtoBoard (parseFenString "4k3/2pp3p/4b3/1np1p1q1/1N1P3P/4BPP1/P3P3/R2QKBNR test")
 
---#time #eval Board.bestMove' TestBoard1 .Black 3
+#time #eval (Board.bestMove TestBoard1 .Black 3).2
 
 -- TODO insgesamt DFS und dann alle mit schlechterem Score wegschmeißen
 
